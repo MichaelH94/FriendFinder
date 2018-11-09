22439b791c0d905142486380b73c5a162eeb2502
zxcv1337x <michaelhaggard94@gmail.com>
2018-11-09 17:03:54 -0600

    initial commit, server.js created

---
+++ server.js
@@ -0,0 +1,26 @@
+// Friend Finder application for KU Coding Bootcamp - Michael Haggard
+
+// Dependencies 
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+require(path.join(__dirname, '/app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
+
+// Bodyparser functions - this is parsing middleware Node library we went over in class
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({
+    extended: true 
+}));
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+})); // Vendor API handling
+
+app.listen(PORT, function() { 
+    console.log("Listening to PORT: " + PORT);
+}); // Listener
